0f15c87631c514de4ba26c70b719caa855fae277
Dilan Morar <DMorar@spartaglobal.com>
2019-12-06 12:28:19 +0000

    if statement in class

--- Spooky_Workshops_class.py
+++ Spooky_Workshops_class.py
@@ -1,10 +1,16 @@
 from Student_Monsters_class import *
 
 class Spooky_Workshops():
-    def __init__(self, subject, staff, student_list, location, duration, course):
-        self.scary_subject = subject
+    def __init__(self, course, staff, location, duration, student = []):
+        self.scary_course = course
         self.staff = staff
-        self.list_student_monsters = student_list
+        self.list_student_monsters = student
         self.location = location
         self.duration = duration
-        self.course = course
+
+    if student1.course == 'scare':
+        name = student1.name
+
+    def add_student(self, name):
+        student_name = self
+        student_name.list_student_monsters.append(name)
