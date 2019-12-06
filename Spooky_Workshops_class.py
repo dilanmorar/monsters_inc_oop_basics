44ebaa16d9447b2ed8dbcf099885835e4c563c80
Dilan Morar <DMorar@spartaglobal.com>
2019-12-06 12:14:00 +0000

    created some students in student monster class

--- Spooky_Workshops_class.py
+++ Spooky_Workshops_class.py
@@ -1,7 +1,10 @@
+from Student_Monsters_class import *
+
 class Spooky_Workshops():
-    def __init__(self, subject, staff, student_list, location, duration):
+    def __init__(self, subject, staff, student_list, location, duration, course):
         self.scary_subject = subject
         self.staff = staff
         self.list_student_monsters = student_list
         self.location = location
         self.duration = duration
+        self.course = course
--- Student_Monsters_class.py
+++ Student_Monsters_class.py
@@ -1,27 +1,33 @@
 from Monster_class import *
 
 class Student_Monsters(Monster):
-    def __init__(self, name, skills, colour, age, student_id, grade):
+    def __init__(self, name, skills, colour, age, student_id, grade, course):
         super().__init__(name, skills, colour, age)
         self.student_id = student_id
         self.grade = grade
+        self.course = course
 
-student1 = Student_Monsters('Dave', '10 eyes', 'blue', '22', '1', 'pass')
+student1 = Student_Monsters('Dave', '10 eyes', 'blue', '22', '1', 'pass', 'scare')
+student2 = Student_Monsters('Mike', 'intelligence', 'green', '18', '2', 'pass', 'scare')
+student3 = Student_Monsters('Randall', 'invisible', 'purple', '20', '3', 'pass', 'scare')
 
-print('Should print student monster name: ')
-print(student1.name)
+# print('Should print student monster name: ')
+# print(student1.name)
+#
+# print('Should print student monster colour: ')
+# print(student1.colour)
+#
+# print('Should print student monster age : ')
+# print(student1.age)
+#
+# print('Should print student monster skills: ')
+# print(student1.skills)
+#
+# print('Should print student monster student id: ')
+# print(student1.student_id)
+#
+# print('Should print student monster grade: ')
+# print(student1.grade)
 
-print('Should print student monster colour: ')
-print(student1.colour)
-
-print('Should print student monster age : ')
-print(student1.age)
-
-print('Should print student monster skills: ')
-print(student1.skills)
-
-print('Should print student monster student id: ')
-print(student1.student_id)
-
-print('Should print student monster grade: ')
-print(student1.grade)
+# print('Should print student monster course: ')
+# print(student1.course)
