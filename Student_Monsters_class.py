aaef490471b2e8be563f75d4c6cd572b92c2f6ae
Dilan Morar <DMorar@spartaglobal.com>
2019-12-06 11:07:25 +0000

    student_monster class runs

--- Student_Monsters_class.py
+++ Student_Monsters_class.py
@@ -1,10 +1,10 @@
 from Monster_class import *
 
 class Student_Monsters(Monster):
-    def __init__(self, name, skills):
-        super().__init__(Student_Monsters, student_id, grade)
+    def __init__(self, name, skills, student_id, grade):
+        super().__init__(name, skills)
         self.student_id = student_id
         self.grade = grade
 
-student1 = Student_Monsters('Dave', '10 eyes')
-print(student1.student_id)
+student1 = Student_Monsters('Dave', '10 eyes', '1', 'pass')
+print(student1.name)
