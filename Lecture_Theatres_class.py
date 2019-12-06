8c1c5fc2297c68e7a3ae8280389ac0a094d04b23
Dilan Morar <DMorar@spartaglobal.com>
2019-12-06 12:05:39 +0000

    created some runs in lecture theatre

--- Lecture_Theatres_class.py
+++ Lecture_Theatres_class.py
@@ -1,7 +1,21 @@
 from Building_class import *
 
 class Lecture_Theatres(Building):
-    def __init__(self, location, floors, rooms, lt_number, capacity):
+    def __init__(self, location, lt_name, capacity, floors = 1, rooms = 1):
         super(). __init__(location, floors, rooms)
-        self.lt_number = lt_number
+        self.floors = floors
+        self.rooms = rooms
+        self.lt_name = lt_name
         self.capacity = capacity
+
+lt1 = Lecture_Theatres('front', 'lt1', '100', )
+print('Should print location of lecture theatre: ')
+print(lt1.location)
+print('Should print name of lecture theatre: ')
+print(lt1.lt_name)
+print('Should print capacity of lecture theatre: ')
+print(lt1.capacity)
+print('Should print floors in lecture theatre: ')
+print(lt1.floors)
+print('Should print rooms in lecture theatre: ')
+print(lt1.rooms)
