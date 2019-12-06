fb653272710953f1fdda421ba4e9081176ff038f
Dilan Morar <DMorar@spartaglobal.com>
2019-12-06 11:55:21 +0000

    created some runs in student monster

--- Student_Monsters_class.py
+++ Student_Monsters_class.py
@@ -1,10 +1,27 @@
 from Monster_class import *
 
 class Student_Monsters(Monster):
-    def __init__(self, name, colour, age,  skills, student_id, grade):
-        super().__init__(name, colour, age,  skills)
+    def __init__(self, name, skills, colour, age, student_id, grade):
+        super().__init__(name, skills, colour, age)
         self.student_id = student_id
         self.grade = grade
 
-student1 = Student_Monsters('Dave', '10 eyes', '1', 'pass')
+student1 = Student_Monsters('Dave', '10 eyes', 'blue', '22', '1', 'pass')
+
+print('Should print student monster name: ')
 print(student1.name)
+
+print('Should print student monster colour: ')
+print(student1.colour)
+
+print('Should print student monster age : ')
+print(student1.age)
+
+print('Should print student monster skills: ')
+print(student1.skills)
+
+print('Should print student monster student id: ')
+print(student1.student_id)
+
+print('Should print student monster grade: ')
+print(student1.grade)
