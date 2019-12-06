c8f17b38de3591cc0fd9fe0e3b5053756005f546
Dilan Morar <DMorar@spartaglobal.com>
2019-12-06 10:37:27 +0000

    made class for spooky_workshops and created a file for student_monsters

---
+++ Spooky_Workshops_class.py
@@ -0,0 +1,6 @@
+class Spooky_Workshops():
+    def __init__(self, subject, staff, student_list, location):
+        self.scary_subject = subject
+        self.staff = staff
+        self.list_student_monsters = student_list
+        self.location = location
